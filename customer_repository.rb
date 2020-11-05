9406bd7bb9b1f7e2fad8cdb828ac18feeba4cf95
Anil Pak <71243066+anLpk@users.noreply.github.com>
2020-11-05 17:52:48 +1100

    Create customer_repository.rb

---
+++ customer_repository.rb
@@ -0,0 +1,47 @@
+require 'csv'
+require_relative '../models/customer'
+
+class CustomerRepository
+  def initialize(csv_file_path)
+    @csv_file_path = csv_file_path
+    @customers = []
+    @next_id = @customers.length + 1
+    add_customers if File.exist?(@csv_file_path)
+  end
+
+  def add_customers
+    csv_options = { headers: :first_row, header_converters: :symbol }
+    CSV.foreach(@csv_file_path, csv_options) do |row|
+      row[:id] = row[:id].to_i
+      @customers << Customer.new(row)
+      @next_id = row[:id] + 1
+    end
+  end
+
+  def create(customer)
+    customer.id = @next_id
+    @next_id += 1
+    @customers << customer
+    save_csv
+  end
+
+  def all
+    @customers
+  end
+
+  def find(id)
+    @customers.find { |customer| customer.id == id }
+  end
+
+  private
+
+  def save_csv
+    csv_options = { headers: :first_row, header_converters: :symbol }
+    CSV.open(@csv_file_path, 'wb', **csv_options) do |csv|
+      csv << ['id', 'name', 'address']
+      @customers.each do |customer|
+        csv << [customer.id, customer.name, customer.address]
+      end
+    end
+  end
+end

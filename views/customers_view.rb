54d6acb3a7cdce9da2c5346c945ddb2b948561c4
Anil Pak <71243066+anLpk@users.noreply.github.com>
2020-11-10 21:47:33 +1100

    Create customers_view.rb

---
+++ views/customers_view.rb
@@ -0,0 +1,14 @@
+# Customers View
+class CustomersView
+  def display(customers)
+    customers.each_with_index do |customer, index|
+      puts "#{index + 1}. #{customer.name} : #{customer.address}"
+    end
+  end
+
+  def ask_user_for(stuff)
+    puts "#{stuff.capitalize}?"
+    print '> '
+    gets.chomp
+  end
+end

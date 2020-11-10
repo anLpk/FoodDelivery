af646268c3fd9b7b693475c89d66d4740ef9d31f
Anil Pak <71243066+anLpk@users.noreply.github.com>
2020-11-10 21:44:16 +1100

    Update and rename order.rb to model/order.rb

--- model/order.rb
+++ model/order.rb
@@ -1,8 +1,13 @@
 class Order
-  attr_reader :customer, :employee
-  attr_accessor :id, :meal
+  attr_accessor :id
+  attr_reader :meal, :customer, :employee
 
   def initialize(attributes = {})
+    # Each order has an id,
+    # a meal,
+    # a customer,
+    # an employee plus a
+    # delivered boolean to record whether or not the order has been delivered.
     @id = attributes[:id]
     @meal = attributes[:meal]
     @customer = attributes[:customer]
